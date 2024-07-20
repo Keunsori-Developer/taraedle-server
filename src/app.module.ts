fefeacf3f97811c34837bc76ca383675cfdb071b
서보민 <sbmsky@gmail.com>
2024-07-20 21:13:19 +0900

    feat : 기본 설정 및 모듈 구성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { WordModule } from './word/word.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, WordModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({ methods: '*', origin: '*' });
+  await app.listen(8000);
 }
+
 bootstrap();
