626295624af9d9a8a6d8d5fb3d7ba57239613bef
sbmcheese <sbm@cheeseade.com>
2024-08-01 16:07:52 +0900

    fix : callback 을 api 문서에서 숨김

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,15 @@ import { Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards } from '@n
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { GooglePayload } from 'src/common/decorator/google-payload.decorator';
-import { ApiBadRequestResponse, ApiBody, ApiExtraModels, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiExcludeEndpoint,
+  ApiExtraModels,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { LogoutResDto, RefreshTokenReqDto } from './dto/request.dto';
 import { TokenResDto } from './dto/response.dto';
 import { ApiGetResponse, ApiPostResponse } from 'src/common/decorator/swagger.decorator';
@@ -16,17 +24,18 @@ export class AuthController {
 
   @ApiOperation({
     summary: ' Google 로그인 요청',
-    description: 'Google 로그인 이후 /auth/login/google/callback 으로 리다이렉트됨',
+    description: '/auth/login/google/callback 으로 리다이렉트 되고, 회원 확인 후 accesstoken과 refreshtoken 반환',
   })
+  @ApiGetResponse(TokenResDto)
   @Get('login/google')
   @UseGuards(AuthGuard('google'))
   async googleLogin() {}
 
+  @ApiExcludeEndpoint()
   @ApiOperation({
     summary: 'Google 로그인 이후 리다이렉트 페이지',
     description: 'Google 로그인 이후 리다이렉트 페이지, 회원 확인 이후 accesstoken과 refreshtoken 반환',
   })
-  @ApiGetResponse(TokenResDto)
   @Get('login/google/callback')
   @UseGuards(AuthGuard('google'))
   async googleLoginCallback(@GooglePayload() payload): Promise<TokenResDto> {
