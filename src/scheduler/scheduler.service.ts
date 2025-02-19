6b06f14f5dad1d733d6efa1be24bac95535c10ee
sbmcheese <sbm@cheeseade.com>
2025-02-19 20:26:42 +0900

    다 지우고 콘솔로그만만

--- src/scheduler/scheduler.service.ts
+++ src/scheduler/scheduler.service.ts
@@ -13,41 +13,42 @@ export class SchedulerService {
     private readonly wordService: WordService,
   ) {}
 
-  // @Cron(CronExpression.EVERY_6_HOURS, { timeZone: 'Asia/Seoul' })
-  // async handleDailyChallenge() {
-  //   const today = new Date();
+  @Cron(CronExpression.EVERY_6_HOURS, { timeZone: 'Asia/Seoul' })
+  async handleDailyChallenge() {
+    console.log('handleDailyChallenge');
+    // const today = new Date();
 
-  //   const todayDailyChallenge = await this.dailyChallengeRepository.findOne({ where: { date: today } });
+    // const todayDailyChallenge = await this.dailyChallengeRepository.findOne({ where: { date: today } });
 
-  //   if (!todayDailyChallenge) {
-  //     const randomDailyChallengeWord = await this.wordService.getRandomWordForDailyChallenge();
+    // if (!todayDailyChallenge) {
+    //   const randomDailyChallengeWord = await this.wordService.getRandomWordForDailyChallenge();
 
-  //     if (!randomDailyChallengeWord) {
-  //       return;
-  //     }
+    //   if (!randomDailyChallengeWord) {
+    //     return;
+    //   }
 
-  //     await this.dailyChallengeRepository.save({
-  //       date: today,
-  //       word: randomDailyChallengeWord,
-  //     });
-  //   }
+    //   await this.dailyChallengeRepository.save({
+    //     date: today,
+    //     word: randomDailyChallengeWord,
+    //   });
+    // }
 
-  //   const tomorrow = new Date();
-  //   tomorrow.setDate(tomorrow.getDate() + 1);
+    // const tomorrow = new Date();
+    // tomorrow.setDate(tomorrow.getDate() + 1);
 
-  //   const tomorrowDailyChallenge = await this.dailyChallengeRepository.findOne({ where: { date: tomorrow } });
+    // const tomorrowDailyChallenge = await this.dailyChallengeRepository.findOne({ where: { date: tomorrow } });
 
-  //   if (!tomorrowDailyChallenge) {
-  //     const randomDailyChallengeWord = await this.wordService.getRandomWordForDailyChallenge();
+    // if (!tomorrowDailyChallenge) {
+    //   const randomDailyChallengeWord = await this.wordService.getRandomWordForDailyChallenge();
 
-  //     if (!randomDailyChallengeWord) {
-  //       return;
-  //     }
+    //   if (!randomDailyChallengeWord) {
+    //     return;
+    //   }
 
-  //     await this.dailyChallengeRepository.save({
-  //       date: tomorrow,
-  //       word: randomDailyChallengeWord,
-  //     });
-  //   }
-  // }
+    //   await this.dailyChallengeRepository.save({
+    //     date: tomorrow,
+    //     word: randomDailyChallengeWord,
+    //   });
+    // }
+  }
 }
